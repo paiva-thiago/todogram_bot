4715c5ab81c1cc6358f4caf95d3008518f2463f3
Thiago Paiva <10606829+paiva-thiago@users.noreply.github.com>
2020-02-11 14:54:58 -0300

    Changing commands

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const bot = new TelegramBot( TOKEN, { polling: true } )
 
  bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, `Olá ${msg.from.first_name}, bem vindo ao ToDoGram!! `)
-    bot.sendMessage(msg.chat.id, "🇧🇷 \nPara adicionar tarefa digite :n <nomeDaTarefa>\nPara listar as tarefas digite :ls \nPara concluir tarefa digite :ok <nomeDaTarefa>");    
-    bot.sendMessage(msg.chat.id, "🇬🇧 \nTo add a task write :n <nameOfTheTask>\nTo list all the tasks write :ls \nTo finish a task write :ok <nameOfTheTask>");    
+    bot.sendMessage(msg.chat.id, "🇧🇷 \nPara adicionar tarefa digite /n <nomeDaTarefa>\nPara listar as tarefas digite /ls \nPara concluir tarefa digite /ok <nomeDaTarefa>");    
+    bot.sendMessage(msg.chat.id, "🇬🇧 \nTo add a task write /n <nameOfTheTask>\nTo list all the tasks write /ls \nTo finish a task write /ok <nameOfTheTask>");    
  })
 
- bot.onText(/^:n/, (msg) => {
+ bot.onText(/\/n/, (msg) => {
     bot.sendMessage(msg.chat.id, 'EM DESENVOLVIMENTO')
     console.log(msg.text)
  })
  
- bot.onText(/^:ls/, (msg) => {
+ bot.onText(/\/ls/, (msg) => {
     bot.sendMessage(msg.chat.id, 'EM DESENVOLVIMENTO')
     console.log(msg.text)
  })
 
- bot.onText(/^:ok/, (msg) => {
+ bot.onText(/\/ok/, (msg) => {
     bot.sendMessage(msg.chat.id, 'EM DESENVOLVIMENTO')
     console.log(msg.text)
  })
