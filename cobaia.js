0c17464b62a9f295e968d31eb2d929918b1631c5
Thiago Paiva <10606829+paiva-thiago@users.noreply.github.com>
2020-02-12 14:08:54 -0300

    Missing only the /ok command

--- cobaia.js
+++
@@ -1,5 +0,0 @@
-const fauna = require('./lib/connect.js')
-const dotenv = require('dotenv')
-dotenv.config()
-
-fauna.getByAuthorAndName(98478268,'Ligar pro dentista')
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TelegramBot = require( `node-telegram-bot-api` )
 const fauna = require('./lib/connect.js')
 const dotenv = require('dotenv')
+const emoji = require('node-emoji').emoji
 dotenv.config()
 
 const TOKEN = process.env.API_KEY
@@ -20,25 +21,60 @@ const createDoc = (from,name)=>{
 }
 
 const bot = new TelegramBot( TOKEN, { polling: true } ) 
+bot.onText(/\/about/, (msg) => {
+   let about = 
+`
+ToDoGram
+https://github.com/paiva-thiago/todogram_bot
 
+Thiago Paiva
+http://www.thiagopaiva.com/
+@paiva_thiago
+`
+   bot.sendMessage(msg.chat.id, about)
+});
  bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, `Olá ${msg.from.first_name}, bem vindo ao ToDoGram!! `)
-    bot.sendMessage(msg.chat.id, "🇧🇷 \nPara adicionar tarefa digite /new <nomeDaTarefa>\nPara listar as tarefas digite /list \nPara concluir tarefa digite /ok <nomeDaTarefa>");    
-    bot.sendMessage(msg.chat.id, "🇬🇧 \nTo add a task write /new <nameOfTheTask>\nTo list all the tasks write /list \nTo finish a task write /ok <nameOfTheTask>");    
+    let welcome = 
+`
+   ${emoji['flag-br']}
+   Olá ${msg.from.first_name}, bem vindo ao ToDoGram!! 
+   Para adicionar tarefa digite /new nomeDaTarefa               
+   Para listar as tarefas digite /list
+   Para concluir tarefa digite /ok nomeDaTarefa
+
+   ${emoji.gb}
+   Hello ${msg.from.first_name}, welcome to ToDoGram!!
+   To add a task write /new nameOfTheTask
+   To list all the tasks write /list
+   To finish a task write /ok nameOfTheTask
+
+   @paiva_thiago 
+
+`
+
+    bot.sendMessage(msg.chat.id, welcome)
+    
  })
 
- bot.onText(/\/new/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'EM DESENVOLVIMENTO')
+ bot.onText(/\/new/, async (msg) => {
+   let name = msg.text.replace(/\/new/,'') 
+   let document = {
+      "from": msg.from.id,
+      "name": name,
+      "createdAt": new Date().toJSON(),
+      "done": false
+    }
+    await fauna.createDocument(document)
+    bot.sendMessage(msg.chat.id, `Tarefa ${name} criada!`)
     console.log(msg)
  })
  
  bot.onText(/\/list/, async (msg) => {
-    //bot.sendMessage(msg.chat.id, 'EM DESENVOLVIMENTO')
     let items = await fauna.getByAuthor(msg.from.id)
     console.log(items)
-    for(let i of items){
-      bot.sendMessage(msg.chat.id, 'TODO:')
-      bot.sendMessage(msg.chat.id, i.name + (i.done?' DONE':''))
+    bot.sendMessage(msg.chat.id, 'TODO:')
+    for(let i of items){      
+      bot.sendMessage(msg.chat.id, i.name + (i.done?emoji.white_check_mark:emoji.x))
     }
     console.log(msg)
  })
